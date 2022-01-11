28334791746355bdef7e582562d576b63aaa70fe
ProcyonDev <96466601+ProcyonDev@users.noreply.github.com>
2022-01-11 20:11:15 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'adamant'
+game 'gta5'
+
+shared_scripts {
+    'config.lua',
+    'utils/xSound/configSound.lua'
+}
+
+client_scripts {
+    "src/client/RMenu.lua",
+    "src/client/menu/RageUI.lua",
+    "src/client/menu/Menu.lua",
+    "src/client/menu/MenuController.lua",
+    "src/client/components/*.lua",
+    "src/client/menu/elements/*.lua",
+    "src/client/menu/items/*.lua",
+    "src/client/menu/panels/*.lua",
+    "src/client/menu/windows/*.lua",
+    'utils/xSound/client/*.lua',
+    'client/*.lua',
+}
+
+
