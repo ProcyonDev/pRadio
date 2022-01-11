92b7fb8857832b2ec2c224e890f723652d38fbb0
ProcyonDev <96466601+ProcyonDev@users.noreply.github.com>
2022-01-11 20:11:49 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,35 @@
+Config = {
+    esx = "::{korioz#0110}::esx:getSharedObject",
+    voice = 'mumble-voip',
+    Active = false,
+    Checked = false,
+    frequencebase = 0,
+    privatefrequence = false,
+    volume = {
+        base = 0,
+    },
+    jobsPrivateRadios = {
+        {jobname = 'police', radio = {1,2,3}},
+        {jobname = 'ambulance', radio = {4,5,6}},
+        {jobname = 'mechanic', radio = {7,8,9}},
+        {jobname = 'bacars', radio = {10,11,12}},
+        {jobname = 'cardealer', radio = {13,14,15}},
+    },
+    notAccessibleRadios = {
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        12,
+        13,
+        14,
+        15
+    }
+}
